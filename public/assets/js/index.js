bee5e9b6d548bd8c306ac56bd2bd9b14f7872c51
jennifer gomez <jennifer23gomez7@gmail.com>
2021-03-17 22:01:21 -0700

    function completed and added delete

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -10,7 +10,7 @@ if (window.location.pathname === '/notes') {
   saveNoteBtn = document.querySelector('.save-note');
   newNoteBtn = document.querySelector('.new-note');
   noteList = document.querySelectorAll('.list-container .list-group');
-}
+};
 
 // Show an element
 const show = (elem) => {
@@ -25,7 +25,7 @@ const hide = (elem) => {
 // activeNote is used to keep track of the note in the textarea
 let activeNote = {};
 
-const getNotes = () =>
+const getNotes = () => 
   fetch('/api/notes', {
     method: 'GET',
     headers: {
@@ -175,6 +175,6 @@ if (window.location.pathname === '/notes') {
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
   noteText.addEventListener('keyup', handleRenderSaveBtn);
-}
+};
 
 getAndRenderNotes();
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const { dbnotes } = require('./Develop/db/db.json');
+const dbnotes = require('./db/db.json');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
 //listen for express
 const app = express();
+const PORT = process.env.PORT || 3001;  //process env for heroku 
+
 // transfer the json data by parse
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // creating the public file to static 
 app.use(express.static('public'));
@@ -20,31 +22,43 @@ app.get('api/notes', (req, res) => {
 //direct the notes to html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-
 });
 
 app.get('/api/notes', (req, res) => {
-    console.log('app.get request for /api/notes');
-    console.log(dbnotes)
+    console.log('get request for /api/notes');
     res.json(dbnotes);
 });
 
 app.post('/api/notes', (req, res) => {
-    res.body.id = dbnotes.length.toString();
-    const notes = req.body;
-    dbnotes.push(notes);
+    console.log(res.body);
+      // req.body is where our incoming content will be
+    req.body.id = dbnotes.length.toString();
+    const note = req.body;
+    dbnotes.push(note);
 
     fs.writeFileSync(
-        path.join(__dirname, './Develop/db/db.json'),
+        path.join(__dirname, './db/db.json'),
         JSON.stringify(dbnotes, null, 2)
-    ); res.json(notes);
+    ); 
+    res.json(note);
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-} );
 
-// chain method to listen 
-app.listen(3001, () => {
-   console.log(`API server now on port 3001!`);
+//bonus delete notes
+app.delete('/api/notes/:id', (req, res) => {
+    dbnotes.filter(dbnote => dbnote.id != req.params.id);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(dbnotes.filter(dbnote => dbnote.id != req.params.id), null, 2)
+    );
+    res.send('Note being deleted');
+});
+    
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+// chain method to listen via port of Heroku
+app.listen(PORT, () => {
+   console.log(`API server now on port ${PORT}`);
 });
